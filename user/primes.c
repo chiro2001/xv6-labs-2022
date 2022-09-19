920cac7d4bdf3fb7aa3889f0709427d0461849cb
Chiro Liang <Chiro2001@163.com>
2022-09-19 21:32:19 +0800

    simplify primes

--- user/primes.c
+++ user/primes.c
@@ -10,10 +10,12 @@
 
 int read_data(int p, void *dst, int len) {
   int l = 0;
-  while (read(p, dst + l, 1) == 1) {
+  int r;
+  while ((r = read(p, dst + l, 1)) == 1) {
     l++;
     if (l == len) return 0;
   }
+  printf("read fd %d error, ret %d\n", p, r);
   return -1;
 }
 
@@ -36,44 +38,51 @@ int main(int argc, char *argv[]) {
     printf("cannot open pipe!\n");
     exit(1);
   }
-  for (int i = 0; i < n - 1; i++) {
-    DATA_TYPE d = (DATA_TYPE)(i + 2);
-    write(W(from), &d, sizeof(DATA_TYPE));
-  }
-  close(W(from));
-  while (1) {
-    printf("from[%d, %d]\n", from[0], from[1]);
-    DATA_TYPE prime;
-    if (read_data(R(from), &prime, sizeof(DATA_TYPE)) != 0) {
-      break;
+  if (fork() != 0) {
+    close(R(from));
+    for (int i = 0; i < n - 1; i++) {
+      DATA_TYPE d = (DATA_TYPE)(i + 2);
+      write(W(from), &d, sizeof(DATA_TYPE));
     }
-    printf("[%d] prime %d\n", getpid(), prime);
-    int to[2] = {0, 0};
-    if (pipe(to) < 0) {
-      printf("cannot open pipe!\n");
-      exit(1);
-    }
-    printf("  to[%d, %d]\n", to[0], to[1]);
-    int pid = fork();
-    if (pid != 0) {
-      // 主进程
-      close(R(to));
-      DATA_TYPE d;
-      while (read_data(R(from), &d, sizeof(DATA_TYPE)) == 0) {
-        if (d % prime == 0) continue;
-        if (write(W(to), &d, sizeof(DATA_TYPE)) < 0) {
-          printf("[%d] write pipe %d failed! data: %d\n", getpid(), W(to), d);
-          exit(1);
-        }
+    wait(0);
+  } else {
+    while (1) {
+      close(W(from));
+      printf("from[%d, %d]\n", from[0], from[1]);
+      DATA_TYPE prime;
+      if (read_data(R(from), &prime, sizeof(DATA_TYPE)) != 0) {
+        close(R(from));
+        break;
       }
-      close(R(from));
-      close(W(to));
-      wait(0);
-      break;
-    } else {
-      // 子进程
-      close(W(to));
-      memcpy(from, to, sizeof(to));
+      printf("[%d] prime %d\n", getpid(), prime);
+      // printf("prime %d\n", prime);
+      int to[2] = {0, 0};
+      if (pipe(to) < 0) {
+        printf("cannot open pipe!\n");
+        exit(1);
+      }
+      printf("  to[%d, %d]\n", to[0], to[1]);
+      if (fork() != 0) {
+        // 主进程
+        close(R(to));
+        DATA_TYPE d;
+        while (read_data(R(from), &d, sizeof(DATA_TYPE)) == 0) {
+          if (d % prime == 0) continue;
+          if (write(W(to), &d, sizeof(DATA_TYPE)) < 0) {
+            printf("[%d] write pipe %d failed! data: %d\n", getpid(), W(to), d);
+            exit(1);
+          }
+        }
+        close(R(from));
+        close(W(to));
+        wait(0);
+        break;
+      } else {
+        // 子进程
+        close(W(to));
+        close(R(from));
+        memcpy(from, to, sizeof(to));
+      }
     }
   }
   exit(0);
