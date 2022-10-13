22289e1b0b253df4fcc06bf5c3e7a074f9acbe67
Chiro Liang <Chiro2001@163.com>
2022-10-13 21:15:31 +0800

    format all files

--- user/xargs.c
+++ user/xargs.c
@@ -1,7 +1,7 @@
+#include "kernel/param.h"
 #include "kernel/stat.h"
 #include "kernel/types.h"
 #include "user/user.h"
-#include "kernel/param.h"
 
 int main(int argc, char *argv[]) {
   char buf[512];
